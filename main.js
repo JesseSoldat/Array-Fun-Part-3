0272bdd5540caacac0c3336ed1147ed97407c860
Jesse Soldat <jsoldat@hotmail.com>
2016-01-11 12:34:53 -0500

    added an if statement to see if the user is married or not and also create a break element to add some spacing between lines

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
 var anchor = document.getElementById('anchor');
 
 var people = [ 
-  ['Joe', 'Marks', 36, 'France'],
-  ['Kate', 'King', 46, 'Germany'],
-  ['Ivan', 'Bowers', 27, 'Australia'],
-  ['Peter', 'Fisher', 39, 'United States'],
-  ['Chad', 'Nelly', 24, 'Russia'],
-  ['Mandy', 'Woods', 41, 'Canada']
+  ['Joe', 'Marks', 36, 'France', [true, 'Male'] ],
+  ['Kate', 'King', 46, 'Germany', [true, 'Female'] ],
+  ['Ivan', 'Bowers', 27, 'Australia', [false, 'Male'] ],
+  ['Peter', 'Fisher', 39, 'United States', [true, 'Male'] ],
+  ['Chad', 'Nelly', 24, 'Russia', [false, 'Male'] ],
+  ['Mandy', 'Woods', 41, 'Canada', [true, 'Female'] ]
 ];
 
 
 
 for(var i = 0; i < people.length; i++) {
+
   var li = document.createElement('li');
   var text = document.createTextNode(people[i][0]);
   li.appendChild(text);
+
+  if(people[i][4][0]) {
+    var married = document.createTextNode(' is married');
+    li.appendChild(married);
+  } else {
+    var notMarried = document.createTextNode(' is not married');
+    li.appendChild(notMarried);
+  }
+  var newLine = document.createElement('br');
   anchor.appendChild(li);
+  anchor.appendChild(newLine);
+
+
 
 
 }
